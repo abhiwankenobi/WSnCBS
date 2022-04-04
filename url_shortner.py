e7cb8841175aad511933e20df010936609c3c2fd
HeymiD <35971205+HeymiD@users.noreply.github.com>
2022-04-04 16:01:49 +0200

    Update url_shortner.py

--- url_shortner.py
+++ url_shortner.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect
+import re
+from urllib.parse import urlparse
+
+def check_url(str1):
+    regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    if re.match(regex, str1) is not None:
+        return True
+    else:
+        return False
+
 
 records = {}
+
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def query_records():
     id = request.args.get('id')
+    if id not in records:
+        return ("", 404)
     if id is not None:
-        return records[id]
+        return (redirect(records[id]),301)
     else:
         return records
 
@@ -19,6 +38,8 @@ def query_records():
 def create_record():
     id = request.args['id']
     url = request.args['url']
+    if id not in records:
+        return ("", 404)
     records[id] = url
     return ""
 
@@ -26,26 +47,20 @@ def create_record():
 def create_short_url():
     url = request.args['url']
     id = str(hash(url))
+    print(check_url(url))
+    if check_url(url) is False:
+        return ("Please enter a valid url", 400)
     records[id] = url
     return id
-    check_url(url)
-
-def check_url(str1):
-    regex = ("((http|https)://)(www.)?" +
-             "[a-zA-Z0-9@:%._\\+~#?&//=]" +
-             "{2,256}\\.[a-z]" +
-             "{2,6}\\b([-a-zA-Z0-9@:%" +
-             "._\\+~#?&//=]*)")
-    url_compile = re.compile(str1)
-    if(re.search(url_compile,str1)):
-        return True
-    else:
-        print("enter correct url")
 
 @app.route('/', methods=['DELETE'])
 def delte_record():
     id = request.args['id']
+    if id not in records:
+        return ("", 404)
     del records[id]
-    return ""
+    return ('', 204)
+
+
 
 app.run(debug=True)
